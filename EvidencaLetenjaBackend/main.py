2c9530103a6d596d34e7cde738b63a760b364725
Benjamin <benjamin.jeran@gmail.com>
2024-10-27 20:01:12 +0100

    Dodan response code
    V kodo je bil dodan response code, ki vrača pravilni status glede na stanje.

--- EvidencaLetenjaBackend/main.py
+++ EvidencaLetenjaBackend/main.py
@@ -1,14 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
+    if name:
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={"message": f"Hello {name}"}
+        )
+    else:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"error": "Name is required"}
+        )
 
